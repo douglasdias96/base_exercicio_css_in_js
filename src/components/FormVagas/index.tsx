015a3c85c40acc8d62bd0045d82dd73c9c50e2d3
DouglasDias <ddc_1996@hotmail.com>
2024-06-07 13:58:54 -0300

    exercicio

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,27 @@
+import { FormEvent, useState } from 'react'
+import Formulario from './style'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Formulario onSubmit={aoEnviarForm}>
+      <input
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <button type="submit">Pesquisar</button>
+    </Formulario>
+  )
+}
+export default FormVagas
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from '../../styles'
+import FormHero from './style'
+
+const Hero = () => (
+  <FormHero>
+    <Container>
+      <h2>As melhores vagas para tecnologia, design e artes visuais.</h2>
+    </Container>
+  </FormHero>
+)
+
+export default Hero
